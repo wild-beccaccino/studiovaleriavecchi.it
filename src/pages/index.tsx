34b3f456997c5496ba97514c5630f386832dcbb6
pietro <pietrograndi@MacBookPro.fritz.box>
2024-11-07 11:12:43 +0100

    feat: clean unused stuff

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import localFont from "next/font/local";
 import styles from "@/styles/Landing.module.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
-const montserrat = localFont({
-  src: "./fonts/Montserrat-VariableFont_wght.ttf",
-  variable: "--font-montserrat",
-  weight: "500 600 400",
-});
 
 export default function Home() {
   return (
